2a0f43d908f07cfa4b92be6c819b5f8b098ce149
Vu Pham <levu2909@gmail.com>
2023-10-28 12:29:45 -0700

    add leaflet to simulation

--- simulation/src/App.js
+++ simulation/src/App.js
@@ -1,8 +1,53 @@
-import logo from "./logo.svg";
 import "./App.css";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  Polyline,
+} from "react-leaflet";
+import { RIDERS, CENTER, ROUTES } from "./const";
+import { useState } from "react";
 
 function App() {
-  return <div className="App">Map</div>;
+  const [runSim, setRunSim] = useState(false),
+    [debug, setDebug] = useState(false);
+
+  return (
+    <div>
+      <MapContainer
+        center={CENTER}
+        zoom={14}
+        scrollWheelZoom={false}
+        zoomControl={false}
+        dragging={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {RIDERS.map(function (rider) {
+          return (
+            <Marker key={rider.email} position={rider.location}>
+              <Popup>
+                {rider.email} <br /> {debug ? rider.location : ""} <br />
+                {debug ? rider.destination : ""}
+              </Popup>
+            </Marker>
+          );
+        })}
+        {ROUTES.map(function (route, index) {
+          return <Polyline key={index} positions={route} />;
+        })}
+      </MapContainer>
+      <button onClick={() => setRunSim(!runSim)}>
+        {runSim ? "Stop Simulation" : "Start Simulation"}
+      </button>
+      <button onClick={() => setDebug(!debug)}>
+        {debug ? "Stop debug" : "Start debug"}
+      </button>
+    </div>
+  );
 }
 
 export default App;
